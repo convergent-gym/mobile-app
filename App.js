7db005ba87c15520a8f88b9f593e8aa891906a8a
wchiew1 <weijet.chiew@yahoo.com>
2023-03-08 17:11:11 -0600

    Added the dev client whatever

--- App.js
+++ App.js
@@ -1,45 +1,85 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as DevMenu from "expo-dev-menu";
 
-const Button = ({ label, onPress }) => (
-  <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
-    <Text style={styles.buttonText}>{label}</Text>
-  </TouchableOpacity>
-);
-
-export default function App() {
+function Map() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button
-        label="Open Dev Menu"
-        onPress={() => {
-          DevMenu.openMenu();
-        }}
-      />
+      <Text style={styles.title}>
+        I'm Da Map
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MomScreen() {
+  const [momVar, updateMom] = useState(0);
+  const colors = ['#F3B5CF', '#FAF5AB', '#02ACA4', '#E581B1'];
+  const words = ['Moms', 'MILFS', 'Cougars', 'Hot'];
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={
+        [ shapes.circle, 
+          {backgroundColor: colors[momVar]},
+          {marginVertical: 50}
+        ]
+      }/>
+      <Button 
+        title = "Your MOM"
+        onPress={() => updateMom((momVar + 1) % colors.length)}
+      />
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Gym Map" component={Map} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Your Mom" component={MomScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  buttonContainer: {
-    backgroundColor: "#4630eb",
-    borderRadius: 4,
-    padding: 12,
-    marginVertical: 10,
-    justifyContent: "center",
-    alignItems: "center",
+  title: {
+    fontSize: 60,
+    fontFamily: 'monospace',
+    color: "#F3B5CF",
   },
-  buttonText: {
-    color: "#ffffff",
-    fontSize: 16,
+  body: {
+    fontSize: 80,
+    fontFamily: 'sans-serif',
   },
 });
+
+const shapes = StyleSheet.create({
+  circle: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+})
